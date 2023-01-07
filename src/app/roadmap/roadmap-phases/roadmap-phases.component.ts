9e6480e86239163d5537919a089685d97c094338
Pranap Sivadasan <pranapsivadasan99@gmail.com>
2023-01-07 18:15:21 +0530

    Added: Roadmap - Phases

--- src/app/roadmap/roadmap-phases/roadmap-phases.component.ts
+++ src/app/roadmap/roadmap-phases/roadmap-phases.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoadmapPhasesComponent implements OnInit {
 
+  phases = [0,1,2,3,4];
+
   constructor() { }
 
   ngOnInit(): void {
