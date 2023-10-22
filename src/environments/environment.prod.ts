a945b1030fe1bcf3f02a13be8ea2bc3da10e955e
Pranap Sivadasan <pranapsivadasan99@gmail.com>
2023-10-22 13:37:29 +0530

    Build 7: Contentful - Team Page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  contentfulSpaceDetails: {
+    spaceId: 'dgnhrf16rcq0',
+    token: 'B2urrHWzYbbQgbbox_-is_4T9qH-zoKrSxbCMRuKb74'
+  },
+  entryIDs : {
+    home: '2mRDFTs34Wq4q189oWNBSG',
+    story: '5fIaQgd56MBs6doONSoi3I',
+    team: '6q4rJrVlo2UA1I8g7wibDH'
+  }
 };
